495998ab8ef8dfad6d3fefaa611f6dcc92e1031c
Huclark <vhuclark@gmail.com>
2024-02-24 22:21:49 +0000

    Add: Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This puppet manifest creates a file in /tmp
+
+file {'/tmp/school':
+    ensure  => present,
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+}
