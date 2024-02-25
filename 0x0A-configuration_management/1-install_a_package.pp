6fa33b8c0b9b4449db151857751ca1946c7230b8
Huclark <vhuclark@gmail.com>
2024-02-25 13:45:20 +0000

    Fix: werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Ensure python3.8 is installed
-package { 'python3.8':
+package { 'python3':
   ensure => 'installed',
 }
 
@@ -7,4 +7,13 @@ package { 'python3.8':
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+# Downgrade Werkzeug to a specific version
+exec { 'downgrade_werkzeug':
+  command => 'pip3 install werkzeug==2.1.1',
+  path    => '/usr/bin',
+  # check if the version is 2.1.1 before downgrading
+  unless  => 'pip3 show werkzeug | grep -q "Version: 2.1.1"',
+  else    => 'Werkzeug is already at version 2.1.1. No need to downgrade',
 }
