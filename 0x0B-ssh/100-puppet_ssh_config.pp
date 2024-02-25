5cfffbd2d2150d39e5e0d1872309da02717e2168
Huclark <vhuclark@gmail.com>
2024-02-25 16:54:31 +0000

    Fix: ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,7 @@
-include stdlib
 # This puppet code updates the ssh configuration to 
 # use the private key ~/.ssh/school without any authentication
 
-file_line {'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/ssh/school',
-  replace => true,
-}
-
-file_line {'Turn off password auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  replace => true,
+file { '/etc/ssh/ssh_config':
+  ensure  => 'present',
+  content => 'IdentityFile ~/.ssh/school\nPasswordAuthentication no\n'
 }
